06116c21cd775dbec76f48b8c45d311b136b51f9
freeman2280 <2280781256@qq.com>
2021-04-27 17:07:40 +0800

    查找算法

---
+++ 二分查找.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+//int binarySearch(int arr[], int low, int high, int target)//递归试
+//{
+//	if (low > high) return 0;
+//	int mid = (low + high) / 2;
+//	if (arr[mid] == target) return mid;
+//	else if (arr[mid] > target)
+//		return binarySearch(arr, low, mid - 1, target);
+//	else
+//		return binarySearch(arr, mid+1, high, target);
+//}
+int binarySearch(int arr[], int low, int high, int target)//循环法
+{
+	if (low > high) return 0;
+	int flag = 0;
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+		if (arr[mid] == target)
+		{
+			flag = 1;
+			return arr[mid];
+		}
+		if (arr[mid] > target)
+			high = mid-1;
+		if (arr[mid] < target)
+			low = mid+1 ;
+	}
+	if (flag == 0)
+		return -1;//没找到
+}
+int main()
+{
+	int arr[10] = { 0 };
+	for (int i = 0; i < 10; i++)
+		arr[i] = i +1;
+	cout << binarySearch(arr, 0, 9, 10) << endl;;
+	system("pause");
+	return 0;
+}
